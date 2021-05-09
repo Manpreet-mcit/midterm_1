5b6b294da9c802b36c29c9521dd5dc72abadd999
Manpreet-mcit <80587258+Manpreet-mcit@users.noreply.github.com>
2021-05-08 21:15:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from resources.routes import initialize_route
 
 app= Flask(__name__)
 api = Api(app)
-
+ 
 DB_ATLAS_URI = "mongodb+srv://cst_user:mcit123@cluster0.vo3fn.mongodb.net/manpreet?retryWrites=true&w=majority"
-
 app.config["MONGODB_HOST"] = DB_ATLAS_URI
 
 initialize_db(app)
