d301c51f8406e951f8dd73372da90d2e460233ca
Manpreet <manpreet.7385@gmail.com>
2021-05-30 10:15:12 +0530

    Updated app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from database.db import initialize_db
 from resources.routes import initialize_route
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 app= Flask(__name__)
 api = Api(app)
@@ -9,8 +11,11 @@ api = Api(app)
 DB_ATLAS_URI = "mongodb+srv://cst_user:mcit123@cluster0.vo3fn.mongodb.net/manpreet?retryWrites=true&w=majority"
 
 app.config["MONGODB_HOST"] = DB_ATLAS_URI
+app.config["JWT_SECRET_KEY"] = 'cst-authorization'
 
 initialize_db(app)
 initialize_route(api)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.run()
